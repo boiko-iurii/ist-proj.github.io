62e33ac79aee0d2debd9267d94023a8915555334
Iurii Boiko <beibel588@gmail.com>
2018-09-26 15:45:01 +0300

    Add files via upload
    upload files

---
+++ js/common.js
@@ -0,0 +1,11 @@
+	$('.slider').slick({
+		autoplay: true,
+		speed: 500,
+		arrows: true,
+		dots: false,
+		infinite: true,
+		cssEase: 'ease-in-out',
+		slidesToShow: 3,
+  	slidesToScroll: 3,
+  	rows: 2
+	});
